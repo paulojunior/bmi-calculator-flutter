e240bf317b538ce2644a13e5e4041093bf11b613
Paulo Junior <jr@live.at>
2021-07-01 22:05:24 -0300

    create the skeleton

--- lib/imput_page.dart
+++ lib/imput_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/widget/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -12,8 +13,26 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Color(0xFF1D1E33)),
+                ReusableCard(colour: Color(0xFF1D1E33)),
+              ],
+            ),
+          ),
+          ReusableCard(colour: Color(0xFF1D1E33)),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Color(0xFF1D1E33)),
+                ReusableCard(colour: Color(0xFF1D1E33)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
